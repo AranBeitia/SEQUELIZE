80542314d43e4091049722d38d04325d45fa80a1
AranBeitia <arantza.beitia@gmail.com>
2023-05-05 19:40:25 +0200

    Feat: middlewares

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const UserController = require('../controllers/UserController')
+const { authentication } = require('../middlewares/authentication')
 
 router.post('/', UserController.create)
 router.get('/', UserController.getAll)
 router.get('/id/:id', UserController.getUserById)
 router.delete('/id/:id', UserController.delete)
 router.put('/id/:id', UserController.update)
+router.post('/login', UserController.login)
+router.delete('/logout', authentication, UserController.logout)
 
 module.exports = router
