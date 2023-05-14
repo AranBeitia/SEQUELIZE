d53384b070bb5e7add284b35106e55368f19c171
AranBeitia <arantza.beitia@gmail.com>
2023-05-14 13:51:17 +0200

    Tests

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const UserController = require('../controllers/UserController')
-const { authentication } = require('../middlewares/authentication')
+const { authentication, isAdmin } = require('../middlewares/authentication')
 
 router.post('/', UserController.create)
-router.get('/', UserController.getAll)
-router.get('/id/:id', UserController.getUserById)
-router.delete('/id/:id', UserController.delete)
-router.put('/id/:id', UserController.update)
 router.post('/login', UserController.login)
-router.delete('/logout', authentication, UserController.logout)
 router.get('/confirm/:emailToken', UserController.confirm)
+router.get('/', authentication, UserController.getAll)
+router.get('/id/:id', authentication, UserController.getUserById)
+router.delete('/id/:id', authentication, isAdmin, UserController.delete)
+router.put('/id/:id', authentication, UserController.update)
+router.delete('/logout', authentication, UserController.logout)
 
 module.exports = router
